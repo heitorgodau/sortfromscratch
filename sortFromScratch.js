63452b28c112ed10cd0cebf837b9e1cf364fdd1e
Heitor Godau <heitorgodau@pop-os.localdomain>
2021-08-08 21:32:49 -0300

    add functions to sort an 2 items array

--- sortFromScratch.js
+++ sortFromScratch.js
@@ -1,21 +1,38 @@
 
-const buildShuffleArray = (length) => {
+const buildShuffleArray = (length, min, max) => {
+  if (max < length) return console.error('Max number must be higher or equal than length')
   const shuffledArray = [];
-  while(shuffledArray.length < length) {
+  while (shuffledArray.length < length) {
     const randomInt = Math.ceil(Math.random() * length)
-    if (!shuffledArray.includes(randomInt))  shuffledArray.push(randomInt)
+    if (!shuffledArray.includes(randomInt)) shuffledArray.push(randomInt)
   }
   return shuffledArray
 }
 
 const splitArr = (arr) => {
-  // const fullArr = [...arr]
-  // const splitedArray = [[],[]]
-  const halfArr = arr.splice(0, fullArr.length / 2)
+  return [[arr.splice(0, arr.length / 2)], arr]
 }
 
-const shuffledArray = buildShuffleArray(100);
-
-const sortArr = (shuffledArray) => {
-  const 
+const sortArr = (shuffledIntArray = []) => {
+  console.log(shuffledIntArray);
+  const sortedArr = []
+  if (shuffledIntArray.length < 1 || shuffledIntArray[0] < shuffledIntArray[1]) {
+    return shuffledIntArray
+  }
+  if (shuffledIntArray[0] > shuffledIntArray[1]) {
+    sortedArr.push(shuffledIntArray[1],shuffledIntArray[0])
+  }
+  return sortedArr
 }
+
+const mergeAndSort = (arr1, arr2) => {
+  const mergedArr = [];
+  for (let i = 1; i < twoDArr.length; i += 1) {
+    twoDArr[i][1]
+  }
+  
+}
+
+
+const shuffledArray = buildShuffleArray(10, 1, 20);
+console.log(sortArr(shuffledArray));
